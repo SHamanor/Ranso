1abeaaa7636ffc568335a6fdef3f1ae42f86faa6
SHamanor <97529050+SHamanor@users.noreply.github.com>
2022-10-21 22:31:25 +0300

    Update gen_pass.py

--- RanSomeWhere_HIT/gen_pass.py
+++ RanSomeWhere_HIT/gen_pass.py
@@ -3,16 +3,6 @@ import string
 
 length=32
 
-# lower = string.ascii_lowercase
-# upper = string.ascii_uppercase
-# num = string.digits
-# symbols = string.punctuation
-
-# all = lower + upper + num + symbols
-
-# temp = random.sample(all,length)
-# password = "".join(temp)
-
 def generate():
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase
