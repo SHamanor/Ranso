26327d0ae2d546ddba2d1243233fdcc18624eac2
SHamanor <97529050+SHamanor@users.noreply.github.com>
2022-10-21 22:28:47 +0300

    Update rsa.py

--- RanSomeWhere_HIT/rsa.py
+++ RanSomeWhere_HIT/rsa.py
@@ -3,7 +3,6 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 from Crypto.PublicKey import RSA
 
-# def rsa_keys():
 # generate private/public key pair
 key = rsa.generate_private_key(backend=default_backend(), public_exponent=65537, \
     key_size=512)
@@ -11,7 +10,7 @@ key = rsa.generate_private_key(backend=default_backend(), public_exponent=65537,
 # get public key in OpenSSH format
 pem_public_key = key.public_key().public_bytes(encoding=serialization.Encoding.PEM,
    format=serialization.PublicFormat.SubjectPublicKeyInfo)
-# pem_public_key = key.public_key()
+
 
 # get private key in PEM container format
 pem = key.private_bytes(encoding=serialization.Encoding.PEM,
@@ -22,10 +21,6 @@ pem = key.private_bytes(encoding=serialization.Encoding.PEM,
 private_key_str = pem.decode('utf-8')
 public_key_str = pem_public_key.decode('utf8')
 
-# print('Private key = ')
-# print(private_key_str)
-# print('Public key = ')
-# print(public_key_str)
 
 private_key_file = open("private_key_str.pem", "wb")
 private_key_file.write(private_key_str.encode())
